384336c36e706512220db37095383b82748a8c37
William Manley <will@stb-tester.com>
2020-04-06 18:26:23 +0100

    tests: Remove unused imports

--- test_stbt_rig.py
+++ test_stbt_rig.py
@@ -1,12 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
 import glob
-import logging
 import os
 import platform
 import random
 import re
-import socket
 import threading
 import time
 from contextlib import contextmanager
