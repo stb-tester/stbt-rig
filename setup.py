1e59e13871e85b8c9b1285130283abef0505ab20
David Röthlisberger <david@stb-tester.com>
2022-06-16 15:35:24 +0100

    PyPI package: Update version to 33.0.0
    In preparation for stb-tester v33 release. We'll version stbt_rig
    according to the latest stb-tester version that it supports.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ manual.
 
 setuptools.setup(
     name="stbt_rig",
-    version="2.0.2",
+    version="33.0.0",
     author="Stb-tester.com Ltd.",
     author_email="support@stb-tester.com",
     description="Library for interacting with the Stb-tester Portal's REST API",
